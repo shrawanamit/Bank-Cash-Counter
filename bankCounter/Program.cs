c456d96292d2f166bf4c2e9d1cb6d88aec9276b9
“shrawanamit” <amitkumar06111@gmail.com>
2020-06-04 10:25:05 +0530

    Added code welcome to bank message

---
+++ bankCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bankCounter
+{
+    class Bank
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO Bank...!!!");
+        }
+    }
+}
