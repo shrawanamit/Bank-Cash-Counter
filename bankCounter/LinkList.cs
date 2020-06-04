189eb8de440fa6b54060a72ddc24d5d6866e0e05
“shrawanamit” <amitkumar06111@gmail.com>
2020-06-04 13:15:08 +0530

    Add coustmer data and print coustumer Data

---
+++ bankCounter/LinkList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bankCounter
+{
+    public class LinkList<T>
+    {
+        class Node
+        {
+            public Node next;
+            public string data;
+            public CostumerRegistration costumerRegistration;
+
+            public Node(CostumerRegistration costumerRegistration)
+            {
+                this.costumerRegistration = costumerRegistration;
+            }
+
+
+            public Node(string costumerRegistration)
+            {
+                data = costumerRegistration;
+                next = null;
+            }
+        }
+
+        Node head, temp;
+        int count = 0;
+        public void AddCostumerToQueue(CostumerRegistration costumerRegistration)
+        {
+                
+            Node node = new Node(costumerRegistration);
+            if (head == null)
+            {
+                head = temp = node;
+            }
+            else
+            {
+                 temp.next = node;
+                temp = node;
+            }
+        }
+        public void PrintCostumerData()
+        {
+            Node currNode =head;
+
+            Console.WriteLine("CostumerData: ");
+
+            
+            while (currNode != null)
+            {
+                // Print the data at current node 
+                Console.WriteLine(currNode.data + " ");
+
+                // Go to next node 
+                currNode = currNode.next;
+            }
+
+        }
+    }
+}
+
--- bankCounter/Program.cs
+++ bankCounter/Program.cs
@@ -7,6 +7,74 @@ namespace bankCounter
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome TO Bank...!!!");
+            string userInput;
+            int choice = 0, amount, accountNumber;
+            Console.Write("1. Add User\n2. Deposit Money\n3. With Drow Money\n4. Check Acount Balance\n");
+            Console.WriteLine("\nSelect Your Choice : ");
+            userInput = Console.ReadLine();
+            choice = Convert.ToInt32(userInput);
+            Console.WriteLine(choice);
+
+            switch (choice)
+            {
+                case 1:
+                    AddUser();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Amount To Be Deposit : ");
+                    userInput = Console.ReadLine();
+                    amount = Convert.ToInt32(userInput);
+                    Deposit(amount);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter Amount To Be Withdrow : ");
+                    userInput = Console.ReadLine();
+                    amount = Convert.ToInt32(userInput);
+                    withdrow(amount);
+                    break;
+                case 4:
+                    Console.WriteLine("Enter Account Number To Search : ");
+                    userInput = Console.ReadLine();
+                    accountNumber = Convert.ToInt32(userInput);
+                    CheckBalance(accountNumber);
+                    break;
+            }
+
+
+        }
+
+        private static void AddUser()
+        {
+            string userName, userAmount, number;
+            uint amount, mobileNumber;
+            Console.WriteLine("Enter User Name : ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter Mobile No : ");
+            number = Console.ReadLine();
+            mobileNumber = Convert.ToUInt32(number);
+            Console.WriteLine("Enter starting Amount : ");
+            userAmount = Console.ReadLine();
+            amount = Convert.ToUInt32(userAmount);
+            LinkList<CostumerRegistration> list = new LinkList<CostumerRegistration>();
+            list.AddCostumerToQueue(new CostumerRegistration(userName, mobileNumber,amount));
+            list.PrintCostumerData();
+
+        }
+
+        private static void CheckBalance(int accountNumber)
+        {
+            
+        }
+
+        private static void Deposit(int amount)
+        {
+           
+        }
+
+        private static void withdrow(int amount)
+        {
+           
         }
     }
+    
 }
